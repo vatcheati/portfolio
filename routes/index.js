53c6ebc40336e18edb47ee548ef8edbff62d028c
vatcheati <vatcheatikian@gmail.com>
2016-03-21 16:38:32 -0700

    filesetup

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'portfolio' });
+  res.render('index', { title: 'home' });
 });
 
-router.get('/about', function(req, res, next) {
-  res.render('about', {title: 'about'} );
-});
-
-router.get('/skills', function(req, res, next) {
-  res.render('skills', {title: 'skills'} );
-});
-
-router.get('/work', function(req, res, next) {
-  res.render('work', {title: 'work'} );
-});
-
-router.get('/contact', function(req, res, next) {
-  res.render('contact', {title: 'contact'} );
+router.get('/movies', function(req, res, next) {
+  res.render('movies', {title: 'movies'} );
 });
 
 module.exports = router;
